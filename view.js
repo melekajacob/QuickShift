8de57d55dac554845421602b6229a6df2606b2f5
Jacob Meleka <melekajacob@yahoo.ca>
2019-07-08 13:22:13 -0400

    Init app

---
+++ view.js
@@ -0,0 +1,8 @@
+const $ = require("jquery");
+var count = 0;
+$("#click-counter").text(count.toString());
+
+$("#countbtn").on("click", () => {
+  count++;
+  $("#click-counter").text(count.toString());
+});
