ce248968ec4552efee959f3b87d4e05a081181db
Jacob Meleka <melekajacob@yahoo.ca>
2019-08-08 22:05:45 -0400

    Worked on formatting

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
+// Modules
 const { app, BrowserWindow } = require("electron");
-const url = require("url");
-const path = require("path");
-const { ipcMain } = require("electron");
 
-let win;
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
 
+// Create a new BrowserWindow when `app` is ready
 function createWindow() {
-  win = new BrowserWindow({ width: 800, height: 600 });
-  win.loadURL(
-    url.format({
-      pathname: path.join(__dirname, "views/home.ejs"),
-      protocol: "file",
-      slashes: true
-    })
-  );
+  mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    webPreferences: { nodeIntegration: true }
+  });
+
+  // Load index.html into the new BrowserWindow
+  mainWindow.loadFile("./views/home.html");
+
+  // Open DevTools - Remove for PRODUCTION!
+  mainWindow.webContents.openDevTools();
+
+  // Listen for window being closed
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
 }
 
+// Electron `app` is ready
 app.on("ready", createWindow);
+
+// Quit when all windows are closed - (Not macOS - Darwin)
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+// When app icon is clicked and app is running, (macOS) recreate the BrowserWindow
+app.on("activate", () => {
+  if (mainWindow === null) createWindow();
+});
--- view.js
+++
@@ -1,8 +0,0 @@
-const $ = require("jquery");
-var count = 0;
-$("#click-counter").text(count.toString());
-
-$("#countbtn").on("click", () => {
-  count++;
-  $("#click-counter").text(count.toString());
-});
